76c4df50ee3fa7830df29b30fb8708031438b735
f20240355-cyber <f20240355@dubai.bits-pilani.ac.in>
2026-04-06 09:51:43 +0400

    Delete Workshop/sports directory

--- Workshop/sports/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import sports_facility
-from . import sports_facility_court
-from . import sports_facility_booking
--- Workshop/sports/models/sports_facility.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, models, fields
-
-class SportsFacility(models.Model):
-    _name = 'sports.facility'
-    _description = 'Sports Facility'
-
-    name = fields.Char(required=True)
